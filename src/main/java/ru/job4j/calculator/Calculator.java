5aca1afec0e92d435765e2c20bd3e3687d2c1986
alkrivosheev <alkrivosheev@gmail.com>
2023-05-07 09:14:59 +0300

    убрал пробелы

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -8,12 +8,10 @@ public class Calculator {
         int six = 6;
         int four = 4;
         int five = 5;
-
         int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
         int onePlusTwo = one + two;
-
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
