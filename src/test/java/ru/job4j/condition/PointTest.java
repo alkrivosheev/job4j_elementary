0bd25e95bb4ee5c413f1ab3c770dd263381e4966
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2023-05-14 02:28:21 +0300

    PointTest

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when00to40then4() {
+        double expected = 4;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 4;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when60to29then2() {
+        double expected = 9.84;
+        int x1 = 6;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 9;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when148to1013then2() {
+        double expected = 6.4;
+        int x1 = 14;
+        int y1 = 8;
+        int x2 = 10;
+        int y2 = 13;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
