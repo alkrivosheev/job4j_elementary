ff3a1e5397df5096d7f8fc04f685de500ef0284b
alkrivosheev <alkrivosheev@gmail.com>
2023-05-18 12:36:13 +0300

    ChessBoard&Test

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -6,7 +6,7 @@ public class ChessBoard {
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
             if (Math.abs(x1 - x2) == Math.abs(y2 - y1)) {
-                rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
+                rsl = Math.abs(y2 - y1);
             }
         }
         return rsl;
