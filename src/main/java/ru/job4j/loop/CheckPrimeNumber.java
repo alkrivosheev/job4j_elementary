106873b8fdcf533be3a683a74ca4eeed35737e43
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2023-05-21 12:54:05 +0300

    Factorial

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = (number < 2)?false: true;
+        for (int i = number - 1; i > 1; i--) {
+            if ((number % i) == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
