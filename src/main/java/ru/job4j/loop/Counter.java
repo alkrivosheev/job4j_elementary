ba3da97494ef8c7b3619cc730cbe8246bfc96797
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2023-05-21 16:03:17 +0300

    Counter

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -12,7 +12,7 @@ public class Counter {
     public static int sumByEven(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) {
-            if (i % 2 == 10) {
+            if (i % 2 == 0) {
                 sum += i;
             }
         }
