71bd428611168a5834a8ac66e74c805b35be3ff2
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2023-05-23 11:29:19 +0300

    ArrayDefinition

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,13 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println(ages.length);
-        System.out.println(surnames.length);
-        System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Святослав Зимин";
+        names[1] = "Ярослав Уиллис";
+        names[2] = "Федор Левин";
+        names[3] = "Антон Хлынов";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
