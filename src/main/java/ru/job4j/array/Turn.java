f018bb5b76be3c6ad8aa068781bda155d0bf867e
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2023-05-24 12:40:11 +0300

    EndsWith&Test

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,13 +2,10 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int tmp = array[i];
             array[i] = array[array.length - 1 - i];
             array[array.length - 1 - i] = tmp;
-            if (i >= (array.length - 1) / 2) {
-                break;
-            }
         }
         return array;
     }
