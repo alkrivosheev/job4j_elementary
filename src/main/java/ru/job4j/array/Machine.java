81609bba637e53a074c66c9665f219ee78e38828
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2023-05-26 09:21:39 +0300

    Machine

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -7,10 +7,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int summ = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (summ >= coins[i]) {
-                summ -= coins[i];
-                rsl[size] = coins[i];
+        for (int coin : coins) {
+            while (summ >= coin) {
+                summ -= coin;
+                rsl[size] = coin;
                 size++;
             }
         }
