1fc24236b3760615ae9b7bafbc6eb1b04d912f77
alkrivosheev <alkrivosheev@gmail.com>
2023-06-06 06:54:01 +0300

    MaxOverload

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,4 +5,16 @@ public class Max {
         int result = left >= right ? left : right;
         return result;
     }
+
+    public static int max(int left, int right, int first) {
+        int result = max(left, right);
+        result = result >= first ? result : first;
+        return result;
+    }
+
+    public static int max(int left, int right, int first, int second) {
+        int result = max(left, right, first);
+        result = result >= second ? result : second;
+        return result;
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,67 @@ public class MaxTest {
         int expected = 8;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        int left = 1;
+        int right = 2;
+        int first = 3;
+        int result = Max.max(left, right, first);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax5To3To8Then8() {
+        int left = 5;
+        int right = 3;
+        int first = 8;
+        int result = Max.max(left, right, first);
+        int expected = 8;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax8To8To8Then8() {
+        int left = 8;
+        int right = 8;
+        int first = 8;
+        int result = Max.max(left, right, first);
+        int expected = 8;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax1To2To3To6Then6() {
+        int left = 1;
+        int right = 2;
+        int first = 3;
+        int second = 6;
+        int result = Max.max(left, right, first, second);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax5To3To8To2Then8() {
+        int left = 5;
+        int right = 3;
+        int first = 8;
+        int second = 2;
+        int result = Max.max(left, right, first, second);
+        int expected = 8;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax8To8To8To8Then8() {
+        int left = 8;
+        int right = 8;
+        int first = 8;
+        int second = 8;
+        int result = Max.max(left, right, first, second);
+        int expected = 8;
+        assertThat(result).isEqualTo(expected);
+    }
 }
