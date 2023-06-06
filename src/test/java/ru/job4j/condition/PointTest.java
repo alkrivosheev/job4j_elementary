74649c35df81846e9f43aeffd3ed62793ccf4687
alkrivosheev <alkrivosheev@gmail.com>
2023-06-06 07:42:36 +0300

    PointOverload

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -40,4 +40,40 @@ public class PointTest {
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void when000to200then2() {
+        double expected = 2;
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(2, 0, 0);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when000to400then4() {
+        double expected = 4;
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(4, 0, 0);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when601to297then11point53() {
+        double expected = 11.53;
+        Point a = new Point(6, 0, 1);
+        Point b = new Point(2, 9, 7);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when1486to101318then13point60() {
+        double expected = 13.60;
+        Point a = new Point(14, 8, 6);
+        Point b = new Point(10, 13, 18);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
 }
