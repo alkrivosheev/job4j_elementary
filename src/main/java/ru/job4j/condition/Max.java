00edd74d6fdab166e8999930ef3b45024c670648
alkrivosheev <alkrivosheev@gmail.com>
2023-06-06 10:59:06 +0300

    Max

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -7,14 +7,10 @@ public class Max {
     }
 
     public static int max(int left, int right, int first) {
-        int result = max(left, right);
-        result = result >= first ? result : first;
-        return result;
+        return max(max(left, right), first);
     }
 
     public static int max(int left, int right, int first, int second) {
-        int result = max(left, right, first);
-        result = result >= second ? result : second;
-        return result;
+        return max(max(left, right, first), second);
     }
 }
