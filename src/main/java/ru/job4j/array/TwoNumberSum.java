3bde92e06f71d03900fd5f593477c87bb4d65b48
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2024-04-17 18:05:31 +0300

    TwoNumberSum

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -2,6 +2,13 @@ package ru.job4j.array;
 
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
+        if (array == null) {
+            throw new IllegalArgumentException("Input array cannot be null");
+        }
+        if (array.length < 2) {
+            throw new IllegalArgumentException("Input array must contain at least two elements");
+        }
+
         int i = 0;
         int j = array.length - 1;
         while (i != j) {
